96d65341cc7df8fc0703f8e7f843ab6cd12839be
Mian Bilal <muhammadbilal12345610@gmail.com>
2024-10-31 23:21:15 +0500

    Update the Meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,9 @@ import { ThemeProvider } from "./provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Adrian's Portfolio",
-  description: "Modern & Minimal JS Mastery Portfolio",
+  title: "SolTok",
+  description:
+    "Discover trending content, manage your Solana assets, and join a vibrant community, all within one powerful app.",
 };
 
 export default function RootLayout({
@@ -19,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/jsm-logo.png" sizes="any" />
+        <link rel="icon" href="/logo.png" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
